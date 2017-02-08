32b7ab0803ce8426f8b68437d83bb1acec02845b
Fleuret-F <fleuret.florian@laposte.net>
2017-02-08 11:21:16 +0100

    bddScript

---
+++ doc/sql/jeuEssaiBddParis.sql
@@ -0,0 +1,117 @@
+----------------------team---------------------------------
+INSERT INTO `paris`.`team` (`id`, `name`, `active`) VALUES 
+	('1', 'PSG', '1'),
+	('2', 'Lyon', '1'),
+	('3', 'Monaco', '1'), 
+	('4', 'Lille', '1'),
+	('5', 'Marseille', '1'),
+	('6', 'Nice', '1');
+
+----------------------competition--------------------------
+INSERT INTO `paris`.`competition` (`id`, `name`, `active`) VALUES 
+	('1', 'Ligue 1', '1'),
+	('2', 'Coupe de la Ligue', '1'),
+	('3', 'Amicale', '1');
+
+------------------competition_has_team----------------------
+INSERT INTO `paris`.`competition_has_team` (`competition_id`, `team_id`) VALUES 
+	('1', '1'),
+	('1', '2'),
+	('1', '3'), 
+	('1', '4'),
+	('1', '5'),
+	('1', '6'),	
+	('2', '3'),
+	('2', '1');
+
+------------------------event--------------------------
+INSERT INTO `paris`.`event` (`id`, `idCompetition`, `idTeamHost`, `idTeamGuest`, `date`, `coteHost`, `coteTie`, `coteGuest`) VALUES
+	('1', '1', '2', '1', '2017-02-23', '1.45', '3.50', '2.25'),
+	('2', '1', '3', '5', '2017-02-23', '2.30', '4.20', '1.25'),
+	('3', '1', '6', '4', '2017-02-23', '4.15', '4.25', '6.45'),
+	('4', '2', '4', '2', '2017-03-01', '1.45', '2.30', '3.25'),
+	('5', '2', '1', '2', '2017-03-01', '8.25', '1.35', '9.85');
+
+----------------------country--------------------------
+INSERT INTO `paris`.`country` (`id`, `name`, `iso`) VALUES 
+	('1', 'France', 'fr'),
+	('2', 'Belgique', 'be'),
+	('3', 'Suisse', 'sw');
+
+----------------------city----------------------------
+INSERT INTO `paris`.`city` (`id`, `name`, `idCountry`) VALUES 
+	('1', 'Rouen', '1'),
+	('2', 'Paris', '1'),
+	('3', 'Marseille', '1'),
+	('4', 'Bourgtherould', '1'),
+	('5', 'Anvers', '2'),
+	('6', 'Bruxelles', '2'),
+	('7', 'Genève', '3');
+
+--------------------adresse----------------------------
+INSERT INTO `paris`.`address` (`id`, `street`, `postcode`, `idCity`) VALUES 
+	('1', '42 rue du pain perdue', '76000', '1'),
+	('2', '4 avenue du maréchal', '75000', '2'),
+	('3', '85 hameau de la vache', '27520', '4'),
+	('4', '48 avenue sauf une fois au chalet', '65854', '5');
+
+----------------------user----------------------------
+INSERT INTO `paris`.`user` (`id`, `lastname`, `firstname`, `email`, `password`, `dateNaissance`, `idAddress`) VALUES 
+	('1', 'Tenance', 'Romain', 'tenance.r@gmail.com', 'azerty', '1988-01-01', '1'),
+	('2', 'Nault', 'Jeromine', 'jerono@mail.fr', 'azerty', '1957-03-18', '3'),
+	('3', 'Dufois', 'Axelle', 'axel58@gmail.com', 'azerty', '1991-06-08', '2'),
+	('4', 'Paletant', 'Jean-Michel', 'jenmich@mail.lol', 'azerty', '1969-06-06', '4');
+
+----------------------opration_type----------------------------
+INSERT INTO `paris`.`opration_type` (`id`, `name`) VALUES 
+	('1', 'Débit Compte'),
+	('2', 'Crédit Compte'),
+	('3', 'Miser'),
+	('4', 'Récuper gains');
+
+----------------------operation----------------------------
+INSERT INTO `paris`.`operation` (`id`, `idOperationType`, `idUser`, `amount`, `date`) VALUES 
+	('1', '2', '1', '150', '2017-02-07'),
+	('2', '3', '1', '10', '2017-02-07'),
+	('3', '3', '2', '5', '2017-02-05'),
+	('4', '3', '3', '20', '2017-01-27'),
+	('5', '3', '4', '40', '2017-01-22');
+
+----------------------bet----------------------------
+INSERT INTO `paris`.`bet` (`idUser`, `idEvent`, `idBetOperation`, `date`, `quotation`) VALUES 
+	('1', '1', '2', '2017-02-07', '2.45'),
+	('2', '2', '3', '2017-02-05', '1.55'),
+	('3', '1', '4', '2017-01-27', '2.35'),
+	('1', '3', '5', '2017-01-22', '2.40');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ doc/sql/scriptBddParis.sql
@@ -0,0 +1,221 @@
+-- MySQL Workbench Synchronization
+-- Generated: 2017-02-08 09:40
+-- Model: New Model
+-- Version: 1.0
+-- Project: Name of the project
+-- Author: Anthony PERIQUET
+
+CREATE SCHEMA `paris` ;
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+ALTER SCHEMA `paris`  DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci ;
+
+CREATE TABLE IF NOT EXISTS `paris`.`user` (
+  `id` INT(11) NOT NULL,
+  `lastname` VARCHAR(100) NULL DEFAULT NULL,
+  `firstname` VARCHAR(60) NULL DEFAULT NULL,
+  `email` VARCHAR(50) NULL DEFAULT NULL,
+  `password` VARCHAR(150) NULL DEFAULT NULL,
+  `dateNaissance` DATE NULL DEFAULT NULL,
+  `idAddress` INT(11) NOT NULL,
+  `account` DECIMAL NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_utilisateur_address1_idx` (`idAddress` ASC),
+  CONSTRAINT `fk_utilisateur_address1`
+    FOREIGN KEY (`idAddress`)
+    REFERENCES `paris`.`address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `paris`.`address` (
+  `id` INT(11) NOT NULL,
+  `street` VARCHAR(45) NULL DEFAULT NULL,
+  `postcode` VARCHAR(6) NULL DEFAULT NULL,
+  `idCity` INT(11) NOT NULL,
+  PRIMARY KEY (`id`, `idCity`),
+  INDEX `fk_address_city1_idx` (`idCity` ASC),
+  CONSTRAINT `fk_address_city1`
+    FOREIGN KEY (`idCity`)
+    REFERENCES `paris`.`city` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `paris`.`city` (
+  `id` INT(11) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `idCountry` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_city_pays_idx` (`idCountry` ASC),
+  CONSTRAINT `fk_city_pays`
+    FOREIGN KEY (`idCountry`)
+    REFERENCES `paris`.`country` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `paris`.`country` (
+  `id` INT(11) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `iso` VARCHAR(5) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `paris`.`competition` (
+  `id` INT(11) NOT NULL,
+  `name` VARCHAR(60) NULL DEFAULT NULL,
+  `active` TINYINT(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `paris`.`team` (
+  `id` INT(11) NOT NULL,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `active` TINYINT(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `paris`.`event` (
+  `id` INT(11) NOT NULL,
+  `idCompetition` INT(11) NOT NULL,
+  `idTeamHost` INT(11) NOT NULL,
+  `idTeamGuest` INT(11) NOT NULL,
+  `date` DATE NOT NULL,
+  `coteHost` DECIMAL(5,2) NULL DEFAULT NULL,
+  `coteTie` DECIMAL(5,2) NULL DEFAULT NULL,
+  `coteGuest` DECIMAL(5,2) NULL DEFAULT NULL,
+  `result` INT(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_event_competition1_idx` (`idCompetition` ASC),
+  INDEX `fk_event_team1_idx` (`idTeamHost` ASC),
+  INDEX `fk_event_team2_idx` (`idTeamGuest` ASC),
+  CONSTRAINT `fk_event_competition1`
+    FOREIGN KEY (`idCompetition`)
+    REFERENCES `paris`.`competition` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_event_team1`
+    FOREIGN KEY (`idTeamHost`)
+    REFERENCES `paris`.`team` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_event_team2`
+    FOREIGN KEY (`idTeamGuest`)
+    REFERENCES `paris`.`team` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `paris`.`competition_has_team` (
+  `competition_id` INT(11) NOT NULL,
+  `team_id` INT(11) NOT NULL,
+  PRIMARY KEY (`competition_id`, `team_id`),
+  INDEX `fk_competition_has_team_team1_idx` (`team_id` ASC),
+  INDEX `fk_competition_has_team_competition1_idx` (`competition_id` ASC),
+  CONSTRAINT `fk_competition_has_team_competition1`
+    FOREIGN KEY (`competition_id`)
+    REFERENCES `paris`.`competition` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_competition_has_team_team1`
+    FOREIGN KEY (`team_id`)
+    REFERENCES `paris`.`team` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `paris`.`operation` (
+  `id` INT(11) NOT NULL,
+  `idOperationType` INT(11) NOT NULL,
+  `idUser` INT(11) NOT NULL,
+  `amount` DECIMAL(18,2) NOT NULL,
+  `date` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_operation_opration_type1_idx` (`idOperationType` ASC),
+  INDEX `fk_operation_user1_idx` (`idUser` ASC),
+  CONSTRAINT `fk_operation_opration_type1`
+    FOREIGN KEY (`idOperationType`)
+    REFERENCES `paris`.`opration_type` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_operation_user1`
+    FOREIGN KEY (`idUser`)
+    REFERENCES `paris`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `paris`.`opration_type` (
+  `id` INT(11) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `paris`.`slideshow_item` (
+  `id` INT(11) NOT NULL,
+  `image` VARCHAR(45) NULL DEFAULT NULL,
+  `idEvent` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_slideshow_item_event1_idx` (`idEvent` ASC),
+  CONSTRAINT `fk_slideshow_item_event1`
+    FOREIGN KEY (`idEvent`)
+    REFERENCES `paris`.`event` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `paris`.`bet` (
+  `idUser` INT(11) NOT NULL,
+  `idEvent` INT(11) NOT NULL,
+  `idBetOperation` INT(11) NOT NULL,
+  `idWinOperation` INT(11) NULL DEFAULT NULL,
+  `date` DATE NOT NULL,
+  `quotation` DECIMAL(5,2) NULL DEFAULT NULL,
+  PRIMARY KEY (`idUser`, `idEvent`, `idBetOperation`),
+  INDEX `fk_user_has_event_event1_idx` (`idEvent` ASC),
+  INDEX `fk_user_has_event_user1_idx` (`idUser` ASC),
+  INDEX `fk_bet_operation1_idx` (`idBetOperation` ASC),
+  INDEX `fk_bet_operation2_idx` (`idWinOperation` ASC),
+  CONSTRAINT `fk_user_has_event_user1`
+    FOREIGN KEY (`idUser`)
+    REFERENCES `paris`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_event_event1`
+    FOREIGN KEY (`idEvent`)
+    REFERENCES `paris`.`event` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_bet_operation1`
+    FOREIGN KEY (`idBetOperation`)
+    REFERENCES `paris`.`operation` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_bet_operation2`
+    FOREIGN KEY (`idWinOperation`)
+    REFERENCES `paris`.`operation` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
