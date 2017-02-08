1f8972b5f10da3e5961f1a3b11339f5c1ce0ba0c
Christophe <christophe.pamart.perso@gmail.com>
2017-02-08 10:06:51 +0100

    Popup pari

--- static/js/init.js
+++ static/js/init.js
@@ -6,3 +6,29 @@ $(function() {
         $('.legal').slideToggle();
     })
 });
+
+/**
+ * Modal pari
+ */
+// Fonction de calcul automatique du gain potentiel
+
+$('.betButton').click(function(){
+    // Intitulé du match
+    var title = $(this).parent().find('.info > .display').text();
+    $('.betModalTitle').text(title);
+
+    // Cote du match
+    var quotation = $(this).text();
+    $('.betModalQuotation > span:last-child').text(quotation);
+
+    // Choix du pari
+    var betOn = $(this).attr('data-bet');
+    $('.betModalBetOn > span:last-child').text(betOn);
+})
+
+$('#betInput').change(function(){
+    var quotation = $('.betModalQuotation > span:last-child').text() * 1;
+    var bet = $('#betInput').val();
+    var sum = Number(quotation) * Number(bet);
+    $('.betModalSum').text(sum);
+})
